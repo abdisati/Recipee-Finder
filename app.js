8b45fdd26fe917981b6f4706cf8a6678f229ca42
Abdi Deresa <abdishegute@gmail.com>
2025-12-27 15:33:51 -0800

    got all the elements using their id

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //get the elements
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const results = document.getElementById("results");
+
+//add event listener
+searchBtn.addEventListener("click", searchRecipe);
+
+searchInput.addEventListener("keydown",(e)=>{
+    if(e.key=="Enter") searchRecipe();
+});
+
